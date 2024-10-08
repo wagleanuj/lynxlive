b3f39579be33103c457b624a41bf120430e9f0d3
anuj <anujwagle@gmail.com>
2024-10-07 22:37:25 -0400

    Reduce redundancy in utiles file

--- utils.js
+++ utils.js
@@ -2,20 +2,14 @@ import fetch from "node-fetch"
 import { config } from "./config.js";
 export const Currency = {
     convert: (amount, currency) => {
+        return Currency.getRate(currency).then(rate => amount * rate);
+    },
+    getRate: (currency) => {
         return fetch(`https://api.currencylayer.com/live?access_key=${config.CURRENCY_LAYER_API_KEY}&currencies=${currency}&source=USD&format=1`)
             .then(res => res.json())
             .then(res => {
                 const usdToCurrencyRate = res.quotes["USD" + currency];
-                const amountInCurrency = amount * usdToCurrencyRate;
-                return amountInCurrency;
+                return usdToCurrencyRate;
             })
-    },
-    getRate: (currency)=>{
-        return fetch(`https://api.currencylayer.com/live?access_key=${config.CURRENCY_LAYER_API_KEY}&currencies=${currency}&source=USD&format=1`)
-        .then(res => res.json())
-        .then(res => {
-            const usdToCurrencyRate = res.quotes["USD" + currency];
-            return usdToCurrencyRate;
-        })
     }
 }
